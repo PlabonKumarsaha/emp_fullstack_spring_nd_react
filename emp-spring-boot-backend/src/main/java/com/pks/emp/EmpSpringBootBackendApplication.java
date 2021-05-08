8e65a04fdd010b2b74869724fbaf0c20fba5f7c1
PKS <pkumarsaha71@gmail.com>
2021-05-09 01:21:56 +0600

    first commit

---
+++ emp-spring-boot-backend/src/main/java/com/pks/emp/EmpSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pks.emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpSpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpSpringBootBackendApplication.class, args);
+	}
+
+}
