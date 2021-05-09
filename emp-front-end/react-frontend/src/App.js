8d21b380ff697d2194b07afc5a8d7dead234a5b1
PKS <pkumarsaha71@gmail.com>
2021-05-09 12:49:07 +0600

    router added

--- emp-front-end/react-frontend/src/App.js
+++ emp-front-end/react-frontend/src/App.js
@@ -3,20 +3,30 @@ import './App.css';
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
+import {BrowserRouter as Router,Route, Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className="container">
-
+<Router>
+      <div className="container">
       <div>
         <HeaderComponent/>
       </div>
-      <ListEmployeeComponent />
+
+      <switch>
+      <Route path = "/" exact component = {ListEmployeeComponent}></Route>
+      <Route path = "/employees" exact component = {ListEmployeeComponent}></Route>
+      <Route path = "/" exact component = {ListEmployeeComponent}></Route>
+      
+      </switch>
 
       <div>
         <FooterComponent/>
       </div>
-
+      </div>
+  
+</Router>
       
     </div>
   );
