ad5553139744a900e54ff977b13556322931f890
PKS <pkumarsaha71@gmail.com>
2021-05-09 12:34:46 +0600

    header and footer added in app.js

--- emp-front-end/react-frontend/src/App.js
+++ emp-front-end/react-frontend/src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
     <div className="container">
-      
+
+      <div>
+        <HeaderComponent/>
+      </div>
       <ListEmployeeComponent />
+
+      <div>
+        <FooterComponent/>
+      </div>
 
       
     </div>
